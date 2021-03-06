efd39ae703393ad2057a561994c8bfb07aeed2b6
Harsh Mathur <harsh.mathur@iiap.res.in>
2021-03-06 14:03:58 +0530

    removed stupid code

--- mpi_plotting_all_profiles.py
+++ mpi_plotting_all_profiles.py
@@ -7,8 +7,6 @@ import os.path
 import sunpy.io
 import h5py
 import time
-import matplotlib
-matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
 from mpi4py import MPI
@@ -52,8 +50,6 @@ fontP.set_size('xx-small')
 
 calib_velocity = 333390.00079943583
 
-fig, axs, obp, obs, inp, ins, temp, vlos, vturb = None, None, None, None, None, None, None, None, None
-
 tuple_map = dict()
 
 l = 0
@@ -90,115 +86,85 @@ class Status(enum.Enum):
 @timeit
 def do_work(k, i, j):
 
-    global fig
-    global axs
-    global obp
-    global obs
-    global inp
-    global ins
-    global temp
-    global vlos
-    global vturb
-
     sys.stdout.write('Processing for k: {}, i:{}, j:{}\n'.format(k, i, j))
 
     try:
         #do something
+        plt.close('all')
+        plt.clf()
+        plt.cla()
+        
+        fig, axs = plt.subplots(2, 2)
+        obp,  = axs[0][0].plot(
+            observed['wav'][indices],
+            observed['profiles'][k, i, j, :, 0][indices],
+            color=red,
+            linewidth=0.5,
+            label='Shock'
+        )
 
-        if not fig:
-            fig, axs = plt.subplots(2, 2)
-            obp,  = axs[0][0].plot(
-                observed['wav'][indices],
-                observed['profiles'][k, i, j, :, 0][indices],
-                color=red,
-                linewidth=0.5,
-                label='Shock'
-            )
+        obs = axs[0][0].scatter(
+            observed['wav'][indices],
+            observed['profiles'][k, i, j, :, 0][indices],
+            color=red,
+            s=size / 4
+            # linewidth=0.5
+            # label='Shock'
+        )
 
-            obs = axs[0][0].scatter(
-                observed['wav'][indices],
-                observed['profiles'][k, i, j, :, 0][indices],
-                color=red,
-                s=size / 4
-                # linewidth=0.5
-                # label='Shock'
-            )
+        # plotting the inverted profile
+        inp, = axs[0][0].plot(
+            observed['wav'][:-1],
+            profiles[0]['profiles'][k, i, j, :-1, 0],
+            color=green,
+            linewidth=0.5,
+            label='Fit'
+        )
 
-            # plotting the inverted profile
-            inp, = axs[0][0].plot(
-                observed['wav'][:-1],
-                profiles[0]['profiles'][k, i, j, :-1, 0],
-                color=green,
-                linewidth=0.5,
-                label='Fit'
-            )
+        ins = axs[0][0].scatter(
+            observed['wav'][:-1],
+            profiles[0]['profiles'][k, i, j, :-1, 0],
+            color=green,
+            # linewidth=0.5,
+            s=size / 4
+            # label='Fit'
+        )
 
-            ins = axs[0][0].scatter(
-                observed['wav'][:-1],
-                profiles[0]['profiles'][k, i, j, :-1, 0],
-                color=green,
-                # linewidth=0.5,
-                s=size / 4
-                # label='Fit'
-            )
+        axs[0][0].set_ylim(0, 0.5)
 
-            axs[0][0].set_ylim(0, 0.5)
+        # plot inverted temperature profile
+        temp, = axs[0][1].plot(
+            atmos[0]['ltau500'][k][i][j],
+            atmos[0]['temp'][k][i][j],
+            color=green,
+            linewidth=0.5
+        )
 
-            # plot inverted temperature profile
-            temp, = axs[0][1].plot(
-                atmos[0]['ltau500'][k][i][j],
-                atmos[0]['temp'][k][i][j],
-                color=green,
-                linewidth=0.5
-            )
+        axs[0][1].set_ylim(4000, 11000)
 
-            axs[0][1].set_ylim(4000, 11000)
+        # plot inverted Vlos profile
+        vlos, = axs[1][0].plot(
+            atmos[0]['ltau500'][k][i][j],
+            (atmos[0]['vlos'][k][i][j] - calib_velocity)/ 1e5,
+            color=green,
+            linewidth=0.5
+        )
 
-            # plot inverted Vlos profile
-            vlos, = axs[1][0].plot(
-                atmos[0]['ltau500'][k][i][j],
-                (atmos[0]['vlos'][k][i][j] - calib_velocity)/ 1e5,
-                color=green,
-                linewidth=0.5
-            )
+        axs[1][0].set_ylim(-20, 20)
 
-            axs[1][0].set_ylim(-20, 20)
+        # plot inverted Vturb profile
+        vturb, = axs[1][1].plot(
+            atmos[0]['ltau500'][k][i][j],
+            atmos[0]['vturb'][k][i][j] / 1e5,
+            color=green,
+            linewidth=0.5
+        )
 
-            # plot inverted Vturb profile
-            vturb, = axs[1][1].plot(
-                atmos[0]['ltau500'][k][i][j],
-                atmos[0]['vturb'][k][i][j] / 1e5,
-                color=green,
-                linewidth=0.5
-            )
+        fig.tight_layout()
 
-            axs[1][1].set_ylim(-10, 10)
+        axs[0][0].legend(loc='upper right', prop=fontP)
 
-            fig.tight_layout()
-
-            axs[0][0].legend(loc='upper right', prop=fontP)
-        else:
-            obp.set_ydata(observed['profiles'][k, i, j, :, 0][indices])
-            obs.set_offsets(np.c_[observed['wav'][indices], observed['profiles'][k, i, j, :, 0][indices]])
-            inp.set_ydata(profiles[0]['profiles'][k, i, j, :-1, 0])
-            ins.set_offsets(np.c_[observed['wav'][:-1], profiles[0]['profiles'][k, i, j, :-1, 0]])
-            temp.set_ydata(atmos[0]['temp'][k][i][j])
-            vlos.set_ydata((atmos[0]['vlos'][k][i][j] - calib_velocity)/ 1e5)
-            vturb.set_ydata(atmos[0]['vturb'][k][i][j] / 1e5)
-            axs[0][0].draw_artist(axs[0][0].patch)
-            axs[0][1].draw_artist(axs[0][1].patch)
-            axs[1][0].draw_artist(axs[1][0].patch)
-            axs[1][1].draw_artist(axs[1][1].patch)
-            axs[0][0].draw_artist(obp)
-            axs[0][0].draw_artist(obs)
-            axs[0][0].draw_artist(inp)
-            axs[0][0].draw_artist(ins)
-            axs[0][1].draw_artist(temp)
-            axs[1][0].draw_artist(vlos)
-            axs[1][1].draw_artist(vturb)
-            fig.canvas.update()
-            fig.canvas.flush_events()
-        plt.savefig(write_path / 'plot_{}_{}_{}.png'.format(k, i + 662, j + 708), format='png', dpi=200)
+        plt.savefig(write_path / 'plot_{}_{}_{}.png'.format(k, i + 662, j + 708), format='png', dpi=1200)
         return Status.Work_done
     except Exception:
         sys.stdout.write('Failed for k: {}, i:{}, j:{}\n'.format(k, i, j))
