f44bb9e61a1c2d12c1f39bd229819328370f151c
Harsh Mathur <harsh.mathur@iiap.res.in>
2021-09-06 11:14:05 +0530

    more

--- make_inversion_result_plots.py
+++ make_inversion_result_plots.py
@@ -1021,7 +1021,7 @@ def make_inversion_density_plots():
             axs.pcolormesh(
                 X1, Y1, H1.T / np.abs(H1).max(),
                 cmap='Blues',
-                alpha=.1
+                alpha=.3
             )
 
             axs.plot(
