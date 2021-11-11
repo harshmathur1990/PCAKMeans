54db11e71d544c5318429b528010c6d4878137e4
Harsh Mathur <harshmathur.1990@gmail.com>
2021-11-11 12:02:56 +0530

    more

--- make_rps_pcolormesh_wholedata_paper.py
+++ make_rps_pcolormesh_wholedata_paper.py
@@ -352,7 +352,7 @@ def make_appendix_plot(rps):
 
         gs = gridspec.GridSpec(6, 3)
 
-        gs.update(left=0.15, right=0.7, bottom=0.08, top=0.99, wspace=0.2, hspace=0.2)
+        gs.update(left=0.15, right=0.8, bottom=0.08, top=0.99, wspace=0.2, hspace=0.2)
 
         k = 0
 
