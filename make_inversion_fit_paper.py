930d121d216fb517ce3a475012465ab3bca223ae
Harsh Mathur <harsh.mathur@iiap.res.in>
2022-04-10 23:30:22 +0530

    more

--- make_inversion_fit_paper.py
+++ make_inversion_fit_paper.py
@@ -12,6 +12,9 @@ base_path = Path('/home/harsh/OsloAnalysis')
 new_kmeans = base_path / 'new_kmeans'
 old_kmeans_file = new_kmeans / 'out_100_0.5_0.5_n_iter_10000_tol_1en5.h5'
 
+inversion_out_file = Path('/home/harsh/OsloAnalysis/new_kmeans/wholedata_inversions/FoVAtoJ.nc')
+inversion_out_file_alt = Path('/home/harsh/OsloAnalysis/new_kmeans/wholedata_inversions/FoVAtoJ_more_frames.h5')
+
 wave_3933 = np.array(
     [
         3932.78952, 3932.85488, 3932.92024, 3932.9856 , 3933.05096,
@@ -242,5 +245,248 @@ def make_inversion_fit_plot(xs, ys, wave_indice, time_steps, ref_x, ref_y, fovNa
     fig.savefig(write_path / 'Profile_fits.pdf', format='pdf', dpi=300)
 
 
+def get_data_for_inversion_fit_plot_alternate(index, ref_x, ref_y, wave_indice, index_alt=None, frame_alt=None, frame_res=None):
+    data = np.zeros((4, 6, 50, 50), dtype=np.float64)
+    obs_profiles = np.zeros((4, 64), dtype=np.float64)
+    syn_profiles = np.zeros((4, 64), dtype=np.float64)
+
+    f = h5py.File(inversion_out_file, 'r')
+    falt = None
+    if index_alt is not None:
+        falt = h5py.File(inversion_out_file_alt, 'r')
+
+    observed_ind = np.array([0, 2, 4])
+
+    synthesized_ind = np.array([1, 3, 5])
+
+    if index_alt is None:
+        data[:, observed_ind] = np.transpose(
+            f['all_profiles'][index * 7: index * 7 + 4, :, :, wave_indice],
+            axes=(0, 3, 1, 2)
+        )
+        data[:, synthesized_ind] = np.transpose(
+            f['syn_profiles'][index * 7: index * 7 + 4, :, :, wave_indice],
+            axes=(0, 3, 1, 2)
+        )
+        obs_profiles[:, :] = f['all_profiles'][index * 7: index * 7 + 4, ref_x, ref_y]
+        syn_profiles[:, :] = f['syn_profiles'][index * 7: index * 7 + 4, ref_x, ref_y]
+    else:
+        for inddd, tindex in enumerate(frame_res):
+            if tindex in frame_alt:
+                indalt = index_alt[frame_alt.index(tindex)]
+                data[inddd, observed_ind] = np.transpose(
+                    falt['all_profiles'][indalt, :, :, wave_indice],
+                    axes=(2, 0, 1)
+                )
+                data[inddd, synthesized_ind] = np.transpose(
+                    falt['syn_profiles'][indalt, :, :, wave_indice],
+                    axes=(2, 0, 1)
+                )
+                obs_profiles[inddd, :] = falt['all_profiles'][indalt, ref_x, ref_y]
+                syn_profiles[inddd, :] = falt['syn_profiles'][indalt, ref_x, ref_y]
+            else:
+                indalt = index * 7 + inddd
+                data[inddd, observed_ind] = np.transpose(
+                    f['all_profiles'][indalt, :, :, wave_indice],
+                    axes=(2, 0, 1)
+                )
+                data[inddd, synthesized_ind] = np.transpose(
+                    f['syn_profiles'][indalt, :, :, wave_indice],
+                    axes=(2, 0, 1)
+                )
+                obs_profiles[inddd, :] = f['all_profiles'][indalt, ref_x, ref_y]
+                syn_profiles[inddd, :] = f['syn_profiles'][indalt, ref_x, ref_y]
+
+    if falt is not None:
+        falt.close()
+
+    f.close()
+
+    return data, obs_profiles, syn_profiles
+
+
+def make_inversion_fit_plot_alternate(fovName, index, ref_x, ref_y, wave_indice, time_steps, index_alt=None, frame_alt=None, frame_res=None):
+
+    data, obs_profiles, syn_profiles = get_data_for_inversion_fit_plot_alternate(
+        index, ref_x, ref_y, wave_indice,
+        index_alt=index_alt, frame_alt=frame_alt, frame_res=frame_res
+    )
+
+    write_path = Path('/home/harsh/Shocks Paper/InversionMaps/')
+
+    time = np.round(
+        np.arange(0, 8.26 * 100, 8.26),
+        2
+    )
+
+    size = plt.rcParams['lines.markersize']
+
+    rela_wave = get_relative_velocity(wave_3933[:-1])
+
+    # vmin = [
+    #     [
+    #         all_profiles[:, :, :, wave_indice[0]].min(),
+    #         syn_profiles[:, :, :, wave_indice[0]].min()
+    #     ],
+    #     [
+    #         all_profiles[:, :, :, wave_indice[1]].min(),
+    #         syn_profiles[:, :, :, wave_indice[1]].min()
+    #     ],
+    #     [
+    #         all_profiles[:, :, :, wave_indice[2]].min(),
+    #         syn_profiles[:, :, :, wave_indice[2]].min()
+    #     ]
+    # ]
+    #
+    # vmax = [
+    #     [
+    #         all_profiles[:, :, :, wave_indice[0]].max(),
+    #         syn_profiles[:, :, :, wave_indice[0]].max()
+    #     ],
+    #     [
+    #         all_profiles[:, :, :, wave_indice[1]].max(),
+    #         syn_profiles[:, :, :, wave_indice[1]].max()
+    #     ],
+    #     [
+    #         all_profiles[:, :, :, wave_indice[2]].max(),
+    #         syn_profiles[:, :, :, wave_indice[2]].max()
+    #     ]
+    # ]
+
+    vmin = data.min()
+
+    vmax = data.max()
+
+    color = ['blue', 'orange', 'brown', 'darkgreen']
+
+    fontsize = 6
+
+    for l in range(3):
+        plt.cla()
+
+        plt.clf()
+
+        plt.close('all')
+
+        fig = plt.figure(figsize=(1.4, 3.41))
+
+        gs = gridspec.GridSpec(4, 2)
+
+        gs.update(left=0, right=1, top=0.91, bottom=0.1, wspace=0.0, hspace=0.0)
+
+        k = 0
+        for i in range(4):
+            for j in range(2):
+                axs = fig.add_subplot(gs[k])
+                print ('{}-{}'.format(i, j + (l * 2)))
+                axs.imshow(
+                    data[i, j + (l * 2)],
+                    cmap='gray',
+                    origin='lower',
+                    vmin=vmin,  # [l][j],
+                    vmax=vmax  # [l][j]
+                )
+                if j == 0:
+                    axs.scatter([ref_y], [ref_x], marker='+', color=color[i])
+                    if i == 0:
+                        axs.set_title(
+                            r'Observed',
+                            fontsize=fontsize
+                        )
+                    axs.text(
+                        0.1, 0.7,
+                        r't={}s'.format(time[time_steps[i]]),
+                        transform=axs.transAxes,
+                        color='white',
+                        fontsize='xx-small'
+                    )
+                else:
+                    if i == 0:
+                        axs.set_title(
+                            r'Synthesized',
+                            fontsize=fontsize
+                        )
+
+                axs.set_xticks([])
+                axs.set_xticklabels([])
+                axs.set_yticks([])
+                axs.set_yticklabels([])
+
+                k += 1
+
+        fig.suptitle(
+            r'Ca II K ${0:+.1f}$ m$\mathrm{{\AA}}$'.format(
+                np.round(
+                    get_relative_velocity(
+                        wave_3933[wave_indice[l]]
+                    ) * 1000,
+                    1
+                )
+            ),
+            fontsize=fontsize
+        )
+        fig.savefig(write_path / 'ROI_{}_CaIIk_fit_{}.pdf'.format(fovName, l), format='pdf', dpi=300)
+
+    plt.cla()
+
+    plt.clf()
+
+    plt.close('all')
+
+    fig, axs = plt.subplots(4, 3, figsize=(2.32, 3.41))
+    plt.subplots_adjust(left=0.17, right=0.99, top=0.91, bottom=0.1, wspace=0.5, hspace=0.15)
+    for i in range(4):
+        for j in range(3):
+            if j == 0:
+                axs[i][j].scatter(wave_3933[:-1] - 3933.682, obs_profiles[i, 0:29], color=color[i], s=size/2, marker='+')
+                axs[i][j].plot(wave_3933[:-1] - 3933.682, syn_profiles[i, 0:29], linestyle='--', color=color[i], linewidth=0.5)
+                axs[i][j].set_ylabel(r'$I/I_{\mathrm{c}}$', fontsize=fontsize)
+                if i == 0:
+                    axs[i][j].set_title(r'Ca II K', fontsize=fontsize)
+                if i == 3:
+                    axs[i][j].set_xlabel(r'$\Delta\lambda\;\mathrm{[\AA]}$', fontsize=fontsize)
+                axs[i][j].set_xticks([-0.5, 0, 0.5])
+                if i == 3:
+                    axs[i][j].set_xticklabels([-0.5, 0, 0.5], fontsize=fontsize)
+                else:
+                    axs[i][j].set_xticklabels([])
+            elif j == 1:
+                axs[i][j].scatter(wave_8542 - 8542.09, obs_profiles[i, 30:30+20], color=color[i], s=size/2, marker='+')
+                axs[i][j].plot(wave_8542 - 8542.09, syn_profiles[i, 30:30+20], linestyle='--', color=color[i], linewidth=0.5)
+                if i == 0:
+                    axs[i][j].set_title(r'Ca II 8542 $\mathrm{\AA}$', fontsize=fontsize)
+                if i == 3:
+                    axs[i][j].set_xlabel(r'$\Delta\lambda\;\mathrm{[\AA]}$', fontsize=fontsize)
+                axs[i][j].set_xticks([-1, 0, 1])
+                if i == 3:
+                    axs[i][j].set_xticklabels([-1, 0, 1], fontsize=fontsize)
+                else:
+                    axs[i][j].set_xticklabels([])
+                axs[i][j].set_xlim(-2.4, 2.4)
+            else:
+                axs[i][j].scatter(wave_6173 - 6173.3340, obs_profiles[i, 30 + 20:30 + 20 + 14], color=color[i], s=size/2, marker='+')
+                axs[i][j].plot(wave_6173 - 6173.3340, syn_profiles[i, 30 + 20:30 + 20 + 14], linestyle='--', color=color[i], linewidth=0.5)
+                if i == 0:
+                    axs[i][j].set_title(r'Fe I 6173 $\mathrm{\AA}$', fontsize=fontsize)
+                if i == 3:
+                    axs[i][j].set_xlabel(r'$\Delta\lambda\;\mathrm{[\AA]}$', fontsize=fontsize)
+                axs[i][j].set_xticks([-0.5, 0, 0.5])
+                if i == 3:
+                    axs[i][j].set_xticklabels([-0.5, 0, 0.5], fontsize=fontsize)
+                else:
+                    axs[i][j].set_xticklabels([])
+                axs[i][j].set_xlim(-0.7, 0.7)
+
+            fmt = lambda x, y: np.round(x, 1)
+            axs[i][j].yaxis.set_major_formatter(mpl.ticker.FuncFormatter(fmt))
+            axs[i][j].yaxis.set_tick_params(labelsize=fontsize)
+
+    # fig.tight_layout()
+
+    fig.savefig(write_path / 'ROI_{}_Profile_fits.pdf'.format(fovName), format='pdf', dpi=300)
+
+
 if __name__ == '__main__':
-    make_inversion_fit_plot(662, 708, np.array([12, 14, 16]), np.array([4, 5, 6, 7]), 25, 18, 'A')
+    # make_inversion_fit_plot(662, 708, np.array([12, 14, 16]), np.array([4, 5, 6, 7]), 25, 18, 'A')
+    make_inversion_fit_plot_alternate('A', 0, 25, 18, np.array([12, 14, 16]), np.array([3, 5, 6, 7]), index_alt=[0, 1, 2], frame_alt=[3, 11, 12], frame_res=[3, 5, 6, 7])
+    make_inversion_fit_plot_alternate('B', 3, 29, 21, np.array([12, 14, 16]), np.array([30, 33, 34, 35]), index_alt=[3, 4, 5, 6], frame_alt=[30, 31, 39, 40], frame_res=[30, 33, 34, 35])
--- make_inversion_result_plots_paper.py
+++ make_inversion_result_plots_paper.py
@@ -1197,7 +1197,7 @@ if __name__ == '__main__':
     # ]
     # plot_data_for_result_plots(0, 4, 6, 18, 'A', index_alt=[0, 1, 2], frame_alt=[3, 11, 12], frame_res=[3, 5, 6, 7, 8, 9, 12])
     # plot_data_for_result_plots(2, 17, 20, 27, 'C', -4, 4)
-    # plot_data_for_result_plots(3, 32, 35, 21, 'B', index_alt=[3, 4, 5, 6], frame_alt=[30, 31, 39, 40], frame_res=[30, 33, 34, 35, 36, 37, 40])
+    plot_data_for_result_plots(3, 32, 35, 21, 'B', index_alt=[3, 4, 5, 6], frame_alt=[30, 31, 39, 40], frame_res=[30, 33, 34, 35, 36, 37, 40])
     # plot_data_for_result_plots(4, 12, 16, 20, 'D', index_alt=[7, 8, 9, 10], frame_alt=[11, 19, 20, 21], frame_res=[11, 14, 15, 16, 17, 18, 21])
     # plot_data_for_result_plots(5, 57, 60, 30, 'G')
     # plot_data_for_result_plots(7, 7, 11, 17, 'E', index_alt=[11, 12, 13, 14], frame_alt=[6, 14, 15, 16], frame_res=[6, 10, 11, 12, 13, 15, 16])
@@ -1213,7 +1213,7 @@ if __name__ == '__main__':
     # make_time_evolution_plots(9, 9, 25, 26, 'F', index_alt=[20, 21, 22], frame_alt=[8, 16, 17], frame_res=[8, 11, 12, 13, 14, 15, 16, 17])
     # make_legend()
     # make_legend_average()
-    make_pre_shock_peak_shock_temp_vlos_scatter_plot()
+    # make_pre_shock_peak_shock_temp_vlos_scatter_plot()
 
     '''
     ## OLD NOT USED
